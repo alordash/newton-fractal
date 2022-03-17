c1a7e9fc47167affbe72d680f7bbf04ff201a2d8
alordash <alordash@mail.ru>
2022-03-17 12:44:20 +0300

    Returned service worker script

---
+++ www/sw.js
@@ -0,0 +1,39 @@
+// Credits: https://dev.to/stefnotch/enabling-coop-coep-without-touching-the-server-2d3n
+
+self.addEventListener("install", function () {
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", function (event) {
+    if (event.request.cache === "only-if-cached" && event.request.mode !== "same-origin") {
+        return;
+    }
+
+    event.respondWith(
+        fetch(event.request)
+            .then(function (response) {
+                if (response.status == 0) {
+                    return response;
+                }
+
+                const newHeaders = new Headers(response.headers);
+                newHeaders.set("Cross-Origin-Embedder-Policy", "require-corp");
+                newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
+
+                const moddedResponse = new Response(response.body, {
+                    status: response.status,
+                    statusText: response.statusText,
+                    headers: newHeaders,
+                });
+
+                return moddedResponse;
+            })
+            .catch(function (e) {
+                console.error(e);
+            })
+    );
+});
