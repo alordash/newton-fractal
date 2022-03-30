b87a214d91e34af9b8813463c11efa31c89b022a
alordash <alordash@mail.ru>
2022-03-30 21:50:38 +0300

    Enabled all fractal presets

--- www/visuals/fractal_presets.js
+++ www/visuals/fractal_presets.js
@@ -99,6 +99,10 @@ const parabolaPreset = () => {
 };
 const fractalPresets = [
     defaultPreset,
+    trianglePreset,
+    rectanglePreset,
+    starPreset,
+    parabolaPreset
 ];
 let startPresetId = randomInt(fractalPresets.length - 1);
 let startPreset = fractalPresets[startPresetId]();
